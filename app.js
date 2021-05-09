fb6138710ed4a4d4fdf569890b84c2bba00558af
shashankarya31 <sshashank.arya@gmail.com>
2021-05-09 19:57:55 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const process = require('./dummyprocess.json');
+
+
+mongoose.connect(
+    'mongodb+srv://' + process.env.MONGO_ATLAS_USER + ':' +
+    process.env.MONGO_ATLAS_PW +
+    '@gate-entry.cdjrb.mongodb.net/test',
+    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
+);
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"            
+    );
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+
+//enter useful routes
+
+//end of useful routes
+
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
+
+ module.exports = app;
